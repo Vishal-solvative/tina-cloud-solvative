4828bb617b622936445ed6814e6e37a928aab6db
Vishal Kumar <vishalkumar@Solvatives-Laptop.local>
2024-10-14 20:03:28 +0530

    fix: trying to add SEO

--- app/[...slug]/page.js
+++ app/[...slug]/page.js
@@ -1,107 +1,122 @@
-import TinaComp from '../../components/TinaComp'
-import {client} from '../../tina/__generated__/client'
-import ClientPage from './client-page'
+import TinaComp from "../../components/TinaComp";
+import { client } from "../../tina/__generated__/client";
+import ClientPage from "./client-page";
 
-export const runtime = 'edge'
+export const runtime = "edge";
 
 // Fetch the data for the page, including SEO fields
-export async function fetchData(slug) {
-    try {
-        if (slug == 'vite.svg.mdx' || slug == 'favicon.ico' || slug == '_next,static,css,app,styles.css.map') {
-            return
-        }
-        const data = await client.queries.page({
-            relativePath: `${slug}.mdx`
-        })
-        if (!data?.data) {
-            return {notFound: true}
-        }
-
-        return {
-            props: {
-                data: JSON.parse(JSON.stringify(data.data)),
-                query: data.query,
-                variables: data.variables,
-                seoFields: data.data.page.seoFields // Return SEO fields as part of props
-            }
-        }
-    } catch (error) {
-        return {notFound: true}
+async function fetchData(slug) {
+  try {
+    if (
+      slug == "vite.svg.mdx" ||
+      slug == "favicon.ico" ||
+      slug == "_next,static,css,app,styles.css.map"
+    ) {
+      return;
     }
+    const data = await client.queries.page({
+      relativePath: `${slug}.mdx`,
+    });
+    if (!data?.data) {
+      return { notFound: true };
+    }
+
+    return {
+      props: {
+        data: JSON.parse(JSON.stringify(data.data)),
+        query: data.query,
+        variables: data.variables,
+        seoFields: data.data.page.seoFields, // Return SEO fields as part of props
+      },
+    };
+  } catch (error) {
+    return { notFound: true };
+  }
 }
 
 // Generate the metadata for the page
-export async function generateMetadata({params}) {
-    const {slug} = params
-    const result = await fetchData(slug)
+export async function generateMetadata({ params }) {
+  const { slug } = params;
+  const result = await fetchData(slug);
 
-    if (result?.notFound) {
-        return {
-            title: 'Page Not Found',
-            description: 'The page you are looking for could not be found.'
-        }
-    }
+  if (result?.notFound) {
+    return {
+      title: "Page Not Found",
+      description: "The page you are looking for could not be found.",
+    };
+  }
 
-    if (
-        slug[0] !== 'vite.svg' &&
-        slug[0] !== 'favicon.ico' &&
-        slug[0] !== '_next,static,css,app,styles.css.map' &&
-        slug[0] !== '_next'
-    ) {
-        const result = await fetchData(slug)
-        const seoFields = result?.props?.seoFields
+  if (
+    slug[0] !== "vite.svg" &&
+    slug[0] !== "favicon.ico" &&
+    slug[0] !== "_next,static,css,app,styles.css.map" &&
+    slug[0] !== "_next"
+  ) {
+    const result = await fetchData(slug);
+    const seoFields = result?.props?.seoFields;
 
-        return {
-            title: seoFields?.metaTitle,
-            description: seoFields?.metaDescription,
-            openGraph: {
-                title: seoFields?.ogTitle || seoFields?.metaTitle,
-                description: seoFields?.ogDescription || seoFields?.metaDescription,
-                url: seoFields?.ogUrl,
-                type: seoFields?.ogType || 'website', // Default to 'website' if not provided
-                site_name: seoFields?.ogSiteName,
-                images: seoFields?.ogImage
-                    ? [
-                          {
-                              url: seoFields.ogImage,
-                              width: seoFields?.ogImageWidth || 600,
-                              height: seoFields?.ogImageHeight || 600,
-                              type: seoFields?.ogImageType
-                          }
-                      ]
-                    : []
-            },
-            twitter: {
-                title: seoFields?.twitterTitle || seoFields?.metaTitle,
-                description: seoFields?.twitterDescription || seoFields?.metaDescription,
-                image: seoFields?.twitterImage
-            },
-            link: [
-                {
-                    rel: 'canonical',
-                    href: seoFields?.canonicalUrl
-                }
+    return {
+      title: seoFields?.metaTitle,
+      description: seoFields?.metaDescription,
+      openGraph: {
+        title: seoFields?.ogTitle || seoFields?.metaTitle,
+        description: seoFields?.ogDescription || seoFields?.metaDescription,
+        url: seoFields?.ogUrl,
+        type: seoFields?.ogType || "website", // Default to 'website' if not provided
+        site_name: seoFields?.ogSiteName,
+        images: seoFields?.ogImage
+          ? [
+              {
+                url: seoFields.ogImage,
+                width: seoFields?.ogImageWidth || 600,
+                height: seoFields?.ogImageHeight || 600,
+                type: seoFields?.ogImageType,
+              },
             ]
-        }
-    }
+          : [],
+      },
+      twitter: {
+        title: seoFields?.twitterTitle || seoFields?.metaTitle,
+        description:
+          seoFields?.twitterDescription || seoFields?.metaDescription,
+        image: seoFields?.twitterImage,
+      },
+      link: [
+        {
+          rel: "canonical",
+          href: seoFields?.canonicalUrl,
+        },
+      ],
+    };
+  }
 }
 
-export default async function Page({params}) {
-    const {slug} = params
-    const result = await fetchData(slug)
+export default async function Page({ params }) {
+  const { slug } = params;
+  const result = await fetchData(slug);
 
-    if (result?.notFound) {
-        // Handle not found case
-        const res = await client.queries.page({
-            relativePath: `pageNotFound.mdx`
-        })
-        return <TinaComp data={JSON.parse(JSON.stringify(res.data))} query={res.query} variables={res.variables} />
-    }
-
+  if (result?.notFound) {
+    // Handle not found case
+    const res = await client.queries.page({
+      relativePath: `pageNotFound.mdx`,
+    });
     return (
-        <>
-            {/* Render the rest of the page */}
-            <ClientPage data={result?.props?.data} query={result?.props?.query} variables={result?.props?.variables} />
-        </>
-    )
+      <TinaComp
+        data={JSON.parse(JSON.stringify(res.data))}
+        query={res.query}
+        variables={res.variables}
+      />
+    );
+  }
+
+  return (
+    <>
+      {/* Render the rest of the page */}
+      <ClientPage
+        data={result?.props?.data}
+        query={result?.props?.query}
+        variables={result?.props?.variables}
+      />
+    </>
+  );
 }
